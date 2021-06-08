afa8a4900633ab826904dea3a71174467d6f1c15
Roman Gelokhov <rgelokhov@leongaming.com>
2021-06-08 22:03:32 +0300

    -travis deploy 2

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mkdir target/github-pages
-git clone --single-branch --branch gh-pages https://romangitlab:$GITHUB_TOKEN@github.com/romangitlab/movie-db.git target/github-pages
+git clone --single-branch --branch gh-pages https://romangitlab:$GITHUB_TOKEN@github.com/romangitlab/movie-db target/github-pages
 cp -R target/github-pages/history target/allure-results/
 mvn allure:report
-cp -R target/site/allure-maven-plugin/* target/github-pages/
+cp -R target/site/allure-maven-plugin/* target/github-pages
 cd target/github-pages
 git config user.name "Travis CI"
 git config user.email "deploy@travis-ci.org"
